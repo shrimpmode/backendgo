9bbb7a665e8028ccc248d14ff79c19aab9269328
shrimpmode <jsilva@uni.pe>
2024-07-28 00:33:20 -0500

    use orm for connection

--- db/db.go
+++ db/db.go
@@ -1,14 +1,22 @@
 package db
 
 import (
-	"database/sql"
 	"fmt"
 	"log"
 	"os"
 
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
+
+type Message struct {
+	ServerID string `gorm:"index"`
+	ChatID   string `gorm:"index"`
+	Content  string
+	ID       uint `gorm:"primaryKey"`
+}
 
 func InitDB() {
 	err := godotenv.Load()
@@ -20,14 +28,13 @@ func InitDB() {
 	password := os.Getenv("DB_PASSWORD")
 	dbname := os.Getenv("DB_NAME")
 
-	dataSourceName := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", user, password, dbname)
+	dataSourceName := fmt.Sprintf("host=localhost port=5432 user=%s password=%s dbname=%s sslmode=disable", user, password, dbname)
 
-	db, err := sql.Open("postgres", dataSourceName)
+	db, err := gorm.Open(postgres.Open(dataSourceName), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
-	if err = db.Ping(); err != nil {
-		panic(err)
-	}
 	fmt.Println("Database connected")
+
+	db.AutoMigrate(&Message{})
 }
