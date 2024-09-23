ed10be12efcca98f0b4386bbf853355004fa425b
shrimpmode <jsilva@uni.pe>
2024-09-23 01:24:29 -0500

    refactor get chat

--- chat/handlers/GetChat.go
+++ chat/handlers/GetChat.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"webserver/auth/jwt"
+	"webserver/middleware"
 	"webserver/models"
 
 	"github.com/gorilla/mux"
@@ -24,4 +26,38 @@ func GetChat(db *gorm.DB) http.HandlerFunc {
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(chats)
 	}
+}
+
+type GetChatHandler struct {
+	db   *gorm.DB
+	user *models.User
+}
+
+func (h *GetChatHandler) SetUser(user *models.User) {
+	h.user = user
+}
+
+func (h *GetChatHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	chats := []models.Chat{}
+	vars := mux.Vars(r)
+
+	where := map[string]interface{}{
+		"server_id": vars["serverId"],
+	}
+	h.db.Where(where).Find(&chats)
+	fmt.Println("Get chats", vars, chats)
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(chats)
+}
+
+func NewGetChatHandler(db *gorm.DB) http.Handler {
+	authenticator := jwt.NewJWTAuthenticator(db)
+	handler := GetChatHandler{db: db}
+
+	return middleware.NewAuthUserMiddleware(
+		&handler,
+		db,
+		authenticator,
+	)
 }
--- chat/routes.go
+++ chat/routes.go
@@ -1,7 +1,6 @@
 package chat
 
 import (
-	"fmt"
 	"net/http"
 	"webserver/chat/handlers"
 
@@ -9,15 +8,7 @@ import (
 	"gorm.io/gorm"
 )
 
-type ChatHandler struct {
-	Value int
-}
-
-func (c *ChatHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(c.Value)
-}
-
 func DefineRoutes(r *mux.Router, db *gorm.DB) {
-	r.Handle("/servers/{serverId}/chats", handlers.GetChat(db)).Methods(http.MethodGet)
-	r.Handle("/v2/chat", handlers.NewChatHandler(db)).Methods("POST")
+	r.Handle("/servers/{serverId}/chats", handlers.NewGetChatHandler(db)).Methods(http.MethodGet)
+	r.Handle("/v2/chat", handlers.NewChatHandler(db)).Methods(http.MethodPost)
 }
