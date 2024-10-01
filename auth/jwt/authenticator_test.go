3f5a60d8d42f9ca04fa1b88a7c51c3cd7597895d
shrimpmode <jsilva@uni.pe>
2024-09-30 21:53:14 -0500

    add more test cases

--- auth/jwt/authenticator_test.go
+++ auth/jwt/authenticator_test.go
@@ -6,23 +6,45 @@ import (
 )
 
 func TestHello(t *testing.T) {
-	want := struct {
-		Token string
-		OK    bool
+	tests := []struct {
+		header string
+		want   string
+		ok     bool
 	}{
-		Token: "example",
-		OK:    true,
+		{
+			header: "Bearer example",
+			want:   "example",
+			ok:     true,
+		},
+		{
+			header: "example",
+			want:   "",
+			ok:     false,
+		},
+		{
+			header: "Bearerexample",
+			want:   "",
+			ok:     false,
+		},
+		{
+			header: "",
+			want:   "",
+			ok:     false,
+		},
 	}
 
 	req, err := http.NewRequest("GET", "testing", nil)
 	if err != nil {
 		t.Fatalf("could not create request for test %v", err)
 	}
-	req.Header.Set("authorization", "Bearer example")
-	authenticator := &JWTAuthenticator{}
-	token, ok := authenticator.GetTokenFromRequest(req)
 
-	if token == "" || !ok {
-		t.Fatalf(" want token:%q,ok: %t | got: %q, %t", want.Token, want.OK, "", false)
+	for _, test := range tests {
+		req.Header.Set("authorization", test.header)
+		authenticator := &JWTAuthenticator{}
+		token, _ := authenticator.GetTokenFromRequest(req)
+
+		if token != test.want {
+			t.Errorf(`token=%q. Want %q`, token, test.want)
+		}
 	}
 }
